e784d96076a67d83bef9a535d94695c2901840dd
Gopal Kumar <gk3910598@gmail.com>
2026-01-04 16:21:56 +0530

    04-01-2026

---
+++ Star_pattern/star_spiral_tower.py
@@ -0,0 +1,38 @@
+# Spiral tower with borders:
+
+#     |||||
+#     |***|
+#     |*1*|
+#     |***|
+#    ||***||
+#    |*2*2*|
+#    ||***||
+#   |||***|||
+#   |*3*3*3*|
+#   |||***|||
+#  ||||***||||
+#  |*4*4*4*4*|
+#  ||||***||||
+
+n = 4
+print(" " * (n-1) + "|"*5)
+# for i in range(1,n+1):
+#     sp = " "*(n-i)
+#     l = ''
+#     c = 1
+#     for j in range(1,3+2*i+1):
+#         if j <= i or j >= 4+i:
+#             l += '|'
+#         else:
+#             l += '*'
+#     k = sp+l
+#     print(k *3, end ="\n")
+
+for j in range(1, n+1):
+    s = " " *(n-j)
+    bar = "|"*j 
+    star = "*"*3
+    num = ('*' + str(j))*j + '*'
+    print(s +bar+star +bar)
+    print(s+"|"+num +"|")
+    print(s+bar+star +bar)
