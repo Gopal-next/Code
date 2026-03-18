2661c1b2fe1b0e7c50a6192e8e014fc3fe738d1a
Gopal Kumar <gk3910598@gmail.com>
2026-03-18 08:51:28 +0530

    18-03-2026(031)

---
+++ Array/Leetcode/031_decompress.py
@@ -0,0 +1,8 @@
+def decompressRLElist(nums):
+    res = []
+    for i in range(0, len(nums), 2):
+        res.extend([nums[i + 1]] * nums[i])
+    return res
+
+nums = [1,2,3,4]
+print(decompressRLElist(nums))
